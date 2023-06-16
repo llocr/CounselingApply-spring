1d89dca10590c10d867c2429ffbe62047242d6f2
llocr <36-96@naver.com>
2023-06-16 13:52:27 +0900

    Initial commit

---
+++ src/main/java/com/llocr/midterm/MidtermAssignmentLeeheesueApplication.java
@@ -0,0 +1,13 @@
+package com.llocr.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermAssignmentLeeheesueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidtermAssignmentLeeheesueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llocr/midterm/MidtermAssignmentLeeheesueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llocr.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermAssignmentLeeheesueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
